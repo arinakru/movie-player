ec908a0668761513ee352768b1062c6a0d40df5a
arinakru <arikr441@gmail.com>
2023-12-29 17:20:35 +0300

    initial commit first-task

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
 
+
+// import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import React, { useState } from 'react';
+import { Typography } from 'antd';
+import { Button } from 'antd/es/radio';
+
+const { Title } = Typography;
 function App() {
+  const [number, setNumber] = useState(0);
+
+  const increaseNumber = () => {
+    setNumber(number + 1);
+  }
+
+  const decreaseNumber = () => {
+    setNumber(number - 1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <Title level = {1} style = {{color: 'blue'}}>Заголовок</Title>
+        <p>{number}</p>
+        <Button onClick={increaseNumber}>Увеличить</Button>
+        <Button onClick={decreaseNumber}>Уменьшить</Button>
       </header>
+
     </div>
-  );
+  )
 }
+
+
+
 
 export default App;
